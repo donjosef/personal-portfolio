9f7a0fef4ce1c0c0f1b6d242219a61e5165652d6
donjoe <giumontanaro@gmail.com>
2018-12-04 19:33:16 +0100

    Toggle navList on click on button

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 /* Load particles-js */
 particlesJS.load('particles-js', 'assets/particles.json');
+
+/* toggle navigation mobile */
+const btn = document.getElementById('toggle-nav');
+btn.addEventListener('click', toggleNav);
+
+function toggleNav() {
+  const navList = document.getElementById('nav-list');
+  if(navList.style.height) {
+    navList.style.height = null;
+  } else {
+    navList.style.height = navList.scrollHeight + 'px';
+  }
+}
