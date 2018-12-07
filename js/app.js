e1dbb80875f8a7d2af23fff38fbb21ce6b48241f
donjoe <giumontanaro@gmail.com>
2018-12-08 00:19:01 +0100

    Implement typewriter with my name

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+const TypeWriter = (function() {
+
+    const dom = {};
+    let text = '';
+    let time = 160;
+
+    function getDomElements() {
+        dom.myName = document.querySelector('.intro .highlight');
+    }
+
+    function type() {
+        const name = dom.myName.dataset.name;
+        text = name.substr(0, text.length + 1);
+
+        dom.myName.textContent = text;
+
+        if(text === name ) {
+          return;
+        }
+
+        setTimeout(() => {
+            type();
+        }, time);
+    }
+
+    function init() {
+       getDomElements();
+       type();
+    }
+
+    return {
+        init: init
+    }
+
+}());
+
+/*Invoke module TypeWriter after a little bit window loads*/
+setTimeout(() => TypeWriter.init(), 400);
+
+
 /* Load particles-js */
 particlesJS.load('particles-js', 'assets/particles.json');
 
